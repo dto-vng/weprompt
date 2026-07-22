5d0deb871e84ac73aeac466bac8f0f524e31e15c
LAP16603 <lap16603@LAP16603s-MacBook-Pro.local>
2026-07-22 17:29:55 +0700

    feat(models): add app operations broker

---
+++ packages/desktop/src/process/services/appOperations/index.ts
@@ -0,0 +1,9 @@
+export { AppOperationsBroker } from './broker';
+export { createTaskRegistry, type AppOperationTaskRegistry } from './taskRegistry';
+export type {
+  AppOperationTaskDefinition,
+  AppOperationsAuditEvent,
+  AppOperationsBrokerDependencies,
+  AppOperationsBrokerOptions,
+  RunTaskOptions,
+} from './types';
