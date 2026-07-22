bcec4da007362912c2d1cb881e9651967d7891e4
minhtq1234 <minhtq4@vng.com.vn>
2026-07-22 17:01:29 +0700

    feat(project): register /project/:id route

--- packages/desktop/src/renderer/components/layout/Router.tsx
+++ packages/desktop/src/renderer/components/layout/Router.tsx
@@ -21,6 +21,7 @@ const ComponentsShowcase = React.lazy(() => import('@renderer/pages/TestShowcase
 const ScheduledTasksPage = React.lazy(() => import('@renderer/pages/cron/ScheduledTasksPage'));
 const TaskDetailPage = React.lazy(() => import('@renderer/pages/cron/ScheduledTasksPage/TaskDetailPage'));
 const TeamIndex = React.lazy(() => import('@renderer/pages/team'));
+const ProjectHome = React.lazy(() => import('@renderer/pages/project'));
 
 const withRouteFallback = (Component: React.LazyExoticComponent<React.ComponentType>) => (
   <Suspense fallback={<AppLoader />}>
@@ -66,6 +67,7 @@ const PanelRoute: React.FC<{ layout: React.ReactElement }> = ({ layout }) => {
           <Route index element={<Navigate to='/guid' replace />} />
           <Route path='/guid' element={withRouteFallback(Guid)} />
           <Route path='/conversation/:id' element={withRouteFallback(Conversation)} />
+          <Route path='/project/:id' element={withRouteFallback(ProjectHome)} />
           <Route
             path='/team/:id'
             element={TEAM_MODE_ENABLED ? withRouteFallback(TeamIndex) : <Navigate to='/guid' replace />}
